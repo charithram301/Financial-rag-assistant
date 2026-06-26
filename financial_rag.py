c36de067f6602ed94d899a998398d2b25ae5cc74
CHARITH RAM BHIMESETTY <charithrambhimesetty@gmail.com>
2026-06-26 15:43:48 +0530

    Update financial_rag.py

--- financial_rag.py
+++ financial_rag.py
@@ -29,30 +29,31 @@ embedding_model = SentenceTransformer(
 )
 
 # Load existing ChromaDB
-client = chromadb.Client()
+vectordb = chromadb.Client()
+collection = vectordb.get_or_create_collection(name = "financial_reports")
 
-collection = client.get_or_create_collection(
-    name="financial_reports"
-)
+
 documents_text = [chunk["text"] for chunk in chunks]
 
-metadatas = [
-    {"company": chunk["company"]}
+metadatas = [{
+    "company": chunk["company"]}
     for chunk in chunks
 ]
 
-ids = [str(i) for i in range(len(chunks))]
 
-embedding = embedding_model.encode(
-    documents_text,
-    convert_to_numpy=True
-)
+ids = [
+    str(i)
+    for i in range(len(chunks))
+]
+
+
 collection.add(
-    ids=ids,
-    documents=documents_text,
-    embeddings=embeddings.tolist(),
-    metadatas=metadatas
+    ids = ids,
+    documents = documents_text,
+    embeddings = embedding.tolist(),
+    metadatas = metadatas
 )
+print("Stored:", len(documents_text))
 
 def retrieve(query, top_k=5):
 
